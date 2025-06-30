930a2c930f1fa009ba555fabb684580ca4f68adb
giulliano <giulliano.scatalon@gmail.com>
2025-06-30 11:06:18 -0300

    [dev] Created health endpoint

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	// "pkg"
 	// types "types/config"
 	"fmt"
+	"handlers"
 	"utils"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	config, _ := utils.GenerateDatabaseConfig()
+	router := gin.Default()
+	router.GET("/health", handlers.GetHealth)
+	router.Run("localhost:8080")
 	fmt.Println(config.Db)
 }
