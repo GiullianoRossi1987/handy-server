25fd7811703c7e2c2d727a4cfa862b80f17653a9
giulliano <giulliano.scatalon@gmail.com>
2025-06-30 11:58:49 -0300

    [dev] Created health endpoint

--- handlers/health.go
+++ handlers/health.go
@@ -9,3 +9,7 @@ import (
 func GetHealth(c *gin.Context) {
 	c.String(http.StatusOK, "server is working owo")
 }
+
+func SetRouter(r gin.IRouter) {
+	r.GET("/health", GetHealth)
+}
--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	// types "types/config"
 	"fmt"
 	"handlers"
-	"utils"
+	types "types/config"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	config, _ := utils.GenerateDatabaseConfig()
-	router := gin.Default()
-	router.GET("/health", handlers.GetHealth)
-	router.Run("localhost:8080")
+	config := types.PsConfig{}
+	config.FromEnv()
 	fmt.Println(config.Db)
+	router := gin.Default()
+	handlers.SetRouter(router)
+	router.Run("localhost:8080")
 }
